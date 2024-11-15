7677527d68ba66c41d0d75d8ee1512a3dd98b39e
Munazzir <mdmunazzir10@gmail.com>
2024-11-15 15:10:17 +0530

    Initial commit

---
+++ src/main/java/com/banks/finserv/FinservApplication.java
@@ -0,0 +1,13 @@
+package com.banks.finserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinservApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banks/finserv/FinservApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banks.finserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinservApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
